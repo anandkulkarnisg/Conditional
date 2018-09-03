931fe6a8653912d53c46c2bb280d9577c8cd9d97
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2018-09-03 08:21:21 +0800

    Anand Kulkarni - 03/08/2018 - Added a small enhancement to the wait with delay function. It had a minor bug.

--- Conditional.cpp
+++ Conditional.cpp
@@ -27,12 +27,13 @@ bool Conditional::wait(const long& waitTimeInMilliSecs)
 	unique_lock<mutex> exclusiveLock(m_mutex);
 	++m_count;
 	m_cond.wait_for(exclusiveLock, std::chrono::milliseconds(waitTimeInMilliSecs), [&](){ return(this->m_status==true); });
-	if(m_status)	
-	{
-		if(--m_count==0)
-			m_status=false;
-	}
-	return(m_status);
+	bool returnStatus=false;
+	if(m_status)
+		returnStatus=m_status;
+
+	if(--m_count==0)
+		m_status=false;
+	return(returnStatus);
 }
 
 // Implement signalOne. This can signal any one thread , but particular thread can not be targeted. currently not supported but can be implemented.
