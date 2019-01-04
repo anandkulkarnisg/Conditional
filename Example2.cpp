a2ab339bee84a23703e0cb31234aa0f110606dd1
Anand Kulkarni <anand.kulkarni.sg@gmail.com>
2019-01-04 18:01:58 +0800

    Anand Kulkarni - 04/01/2019 18:01:58:658 Asia/Singapore(+08,+0800) - Added eclipse import and changes to Example2 which was having lockdown issue. Still need to investigate and fix the testCase..

--- Example2.cpp
+++ Example2.cpp
@@ -7,6 +7,7 @@
 #include<chrono>
 #include<set>
 #include<cassert>
+#include<atomic>
 
 #include "Conditional.h"
 
@@ -15,61 +16,71 @@ using namespace std;
 Conditional cond;
 mutex cout_mutex;
 const int demoThreadPoolSize = 10;
-const int demoLoopCount = 1000;
+const int demoLoopCount = 10000;
+atomic<int> counter=demoThreadPoolSize;
+
 void printMessage(string message) // remove const ref to facilitate std::move
 {
-	unique_lock<mutex> exclusiveLock(cout_mutex);
-	cout << message << endl;
+  unique_lock<mutex> exclusiveLock(cout_mutex);
+  cout << message << endl;
 }
 
 string getThreadId()
 {
-	auto myid = this_thread::get_id();
-	stringstream ss;
-	ss << myid;
-	string resultString = ss.str();
-	return resultString;
+  auto myid = this_thread::get_id();
+  stringstream ss;
+  ss << myid;
+  string resultString = ss.str();
+  return resultString;
 }
 
 void waitThread()
 {
-	cond.wait();
+  cond.wait();
+  --counter;
 }
 
 int main(int argc, char* argv[])
 {
-	set<string> testResultSet;
-	
-	for(unsigned int i=0; i<demoLoopCount; ++i)
-	{
-		// create and reserve a thread pool.	
-		vector<thread> threadPool;
-		threadPool.reserve(demoThreadPoolSize);
+  set<string> testResultSet;
 
-		// spwan the threads.
-		for(unsigned int i=0; i<demoThreadPoolSize;++i)
-			threadPool.emplace_back(thread(&waitThread));
+  for(unsigned int i=0; i<demoLoopCount; ++i)
+  {
+    // create and reserve a thread pool.	
+    vector<thread> threadPool;
+    threadPool.reserve(demoThreadPoolSize);
 
-		// Now wait for around 10 seconds and signal all of them to quit.
-		this_thread::sleep_for(chrono::milliseconds(10));
+    // spwan the threads.
+    for(unsigned int i=0; i<demoThreadPoolSize;++i)
+      threadPool.emplace_back(thread(&waitThread));
 
-		cond.signalAll();
+    // Now wait for around 10 seconds and signal all of them to quit.
+    this_thread::sleep_for(chrono::milliseconds(2));
+    while(counter!=0)
+    {
+      cond.signalAll();
+    }
+    counter=demoThreadPoolSize;
 
-		// Join all the threads.	
-		for_each(threadPool.begin(), threadPool.end(), [&](thread& threadItem) { threadItem.join(); });
+    // Join all the threads.	
+    for_each(threadPool.begin(), threadPool.end(), [&](thread& threadItem) { threadItem.join(); });
 
-		// Insert the resulting string status to the set.
-		testResultSet.insert(cond.toString());
-	
-		// Show something on the stdout for progress.
-		cout << "Finished the testing case : " << i << endl;
-	}
+    // Insert the resulting string status to the set.
+    testResultSet.insert(cond.toString());
 
-	//assert (testResultSet.size()==1); problem with assert is it will dump core and exit if assertion fails.
-	
-	if(testResultSet.size() == 1)
-		cout << "Test case result : Successfull!!" << endl;
-	else
-		cout << "Test case failed. There is a race somewhere" << endl;
-	return 0;
+    // Show something on the stdout for progress.
+    cout << "Finished the testing case : " << i << endl;
+  }
+
+  //assert (testResultSet.size()==1); problem with assert is it will dump core and exit if assertion fails.
+
+  if(testResultSet.size() == 1)
+    cout << "Test case result : Successfull!!" << endl;
+  else
+  {
+    cout << "Test case failed. There is a race somewhere" << endl;
+    for(const auto& iter : testResultSet)
+      cout << iter << endl;
+  }
+  return 0;
 }
